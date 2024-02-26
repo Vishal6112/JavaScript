489e7503c3120767d0cf0dbe9d4ae62b5decdc14
Vishal6112 <160302800+Vishal6112@users.noreply.github.com>
2024-02-26 16:06:13 +0000

    test

--- basic/test.js
+++ basic/test.js
@@ -1,1 +1,13 @@
 console.log("Vishal")
+
+const empId=1
+let empName="vishal"
+var EmpAdd="masur"
+
+
+
+
+//empId=2   we cannot change constatnt variable
+empName="sham"
+EmpAdd="karad"
+console.table([empId,empName,EmpAdd])
