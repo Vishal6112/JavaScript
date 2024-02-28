5943c705a8fa7bdec157bf6059b3c666160834e4
Vishal6112 <160302800+Vishal6112@users.noreply.github.com>
2024-02-28 12:05:59 +0000

    datatypes

---
+++ basic/datatypes.js
@@ -0,0 +1,13 @@
+
+let age=12
+console.log(typeof age)
+
+let name="vishal"
+console.log(typeof name)
+
+const a=10
+
+console.log(typeof a)
+
+console.log(typeof null)
+console.log(typeof undefined)
