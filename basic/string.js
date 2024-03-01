6b449543f058f869d9986b55563d038b25c24a86
Vishal6112 <160302800+Vishal6112@users.noreply.github.com>
2024-03-01 06:19:39 +0000

    string function

---
+++ basic/string.js
@@ -0,0 +1,21 @@
+//string decleration 
+ //type 1
+let a1="Vishal"
+let a2="pokale"
+
+//type 2
+
+const abc=new String("vishal")
+
+console.log(abc)
+
+
+//String concatenation 
+console.log(a1+a2)
+
+//string interpolation
+
+let ab ="vikas"
+let bc="datta"
+
+console.log(`${ab} and ${bc} are brothers`)
